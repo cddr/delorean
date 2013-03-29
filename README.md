aaacf69223caa6501a1906f8ff343921d3e8409e
Andy Chambers <achambers.home@gmail.com>
2013-03-29 01:47:15 +0000

    Improve the tests

--- README.md
+++ README.md
@@ -4,21 +4,28 @@ A simple set of macros for mocking time
 
 ## Usage
 
-    (with-frozen-clock (local-time:now)
-      (sleep 2)
-      (local-time:timestamp= (local-time:now) point-in-time))
+    (deftest delorean ()
+      (let ((point-in-time (local-time:now)))
+        (with-frozen-clock point-in-time
+          (sleep 2)
+          (is (local-time:timestamp= (local-time:now) point-in-time))))
     
-    (let ((same-time-tomorrow (local-time:timestamp+ (local-time:now) 1 :day)))
-      (with-shifted-clock same-time-tomorrow
-        (sleep 2)
-        (local-time:timestamp= (local-time:now)
-          (local-time:timestamp+ same-time-tomorrow (local-time:now) 2 :seconds))))
     
-    (let ((now (local-time:now)))
-      (with-scaled-clock (2)
-        (sleep 2)
-        (local-time:timestamp= (local-time:timestamp+ now 4 :seconds))))
+      (let ((same-time-tomorrow (local-time:timestamp+ (local-time:now) 1 :day)))
+        (with-shifted-clock same-time-tomorrow
+          (sleep 2)
+          (is (local-time:timestamp= (local-time:now)
+               (local-time:timestamp-minimize-part
+                (local-time:timestamp+ same-time-tomorrow 2 :sec) :nsec)))))
     
+      
+      (let ((now (local-time:now)))
+        (with-scaled-clock 2
+          (sleep 2)
+          (is (local-time:timestamp= (local-time:now)
+                (local-time:timestamp-minimize-part
+                  (local-time:timestamp+ now 4 :sec) :nsec))))))
+
 The respective clocks can be nested arbitrarily inside one-another.
 
 ## Bugs?
--- tests.lisp
+++ tests.lisp
@@ -4,31 +4,29 @@
 
 (in-package :delorean-test)
 
-(test delorean-frozen
-  (let ((past (timestamp- (now) 1 :year))
-	(future (timestamp+ (now) 1 :year))
-	(now (now)))
-      (is (timestamp= (with-frozen-clock past
-			(now))
-		      past))
-      (is (timestamp= (with-frozen-clock future
-			(now))
-		      future))
-      (is (timestamp= (with-frozen-clock now
-			(sleep 1)
-			(now))
-		      now))))
+;; makes the indentation better in emacs
+(defmacro deftest (name (&rest rest) &body body)
+  (declare (ignore rest))
+  `(test ,name ,@body))
+
+(deftest delorean ()
+  (let ((point-in-time (local-time:now)))
+    (with-frozen-clock point-in-time
+      (sleep 2)
+      (is (local-time:timestamp= (local-time:now) point-in-time))))
 
 
-;; (test delorean-shift
-;;   (let ((past (timestamp- (now) 1 :year))
-;; 	(future (timestamp+ (now) 1 :year))
-;; 	(now (now)))
-;;     (is (timestamp= (with-shifted-clock past
-;; 		      (
+  (let ((same-time-tomorrow (local-time:timestamp+ (local-time:now) 1 :day)))
+    (with-shifted-clock same-time-tomorrow
+      (sleep 2)
+      (is (local-time:timestamp= (local-time:now)
+           (local-time:timestamp-minimize-part
+            (local-time:timestamp+ same-time-tomorrow 2 :sec) :nsec)))))
 
-;; (test timecop-traveller
   
-	    
-
-;; (explain! (run 'delorean-frozen))
+  (let ((now (local-time:now)))
+    (with-scaled-clock 2
+      (sleep 2)
+      (is (local-time:timestamp= (local-time:now)
+            (local-time:timestamp-minimize-part
+              (local-time:timestamp+ now 4 :sec) :nsec))))))
